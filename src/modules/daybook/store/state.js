9d1b3d528fcc7e7b8acd79db4e4c92e0800d5326
@FranCarn <carnevalefranco99@gmail.com>
2024-02-20 12:26:58 -0300

    fake data ready

--- src/modules/daybook/store/state.js
+++ src/modules/daybook/store/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: true,
-  entries: [],
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore, quae eveniet! Repellat, repudiandae iste illo adipisci quibusdam fugiat cum pariatur quae laudantium itaque voluptatum dolorum aut hic ex quo. Placeat.",
+      picture: null, // https://image-url.com
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: "Dolore, quae eveniet! Repellat, repudiandae iste illo adipisci quibusdam fugiat cum pariatur quae laudantium itaque voluptatum dolorum aut hic ex quo. Placeat.",
+      picture: null, // https://image-url.com
+    },
+    {
+      id: new Date().getTime() - 1000,
+      date: new Date().toDateString(),
+      text: "Voluptatum dolorum aut hic ex quo. Placeat.",
+      picture: null, // https://image-url.com
+    },
+  ],
 });
