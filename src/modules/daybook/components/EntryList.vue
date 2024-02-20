5da142af167cd1560e21cde73406d708dbb04aae
Franco Carnevale <112149397+FranCarn@users.noreply.github.com>
2024-02-20 12:54:40 -0300

    mapGetters - get entries by term ready (#1)

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="entry-list-container">
     <div class="px-2 pt-2">
-      <input type="text" class="form-control" placeholder="Search entry" />
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Search entry"
+        v-model="term"
+      />
     </div>
     <div class="entry-scroll-area">
-      <h2 v-for="item in 100" :key="item"><Entry /></h2>
+      <h2 v-for="item in entriesByTerm" :key="item"><Entry /></h2>
     </div>
   </div>
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
 export default {
   components: {
     Entry: defineAsyncComponent(() => import("./EntryComponent")),
+  },
+  computed: {
+    ...mapGetters("journal", ["getEntriesByTerm"]),
+    entriesByTerm() {
+      return this.getEntriesByTerm(this.term);
+    },
+  },
+  data() {
+    return {
+      term: "",
+    };
   },
 };
 </script>
--- src/modules/daybook/store/getters.js
+++ src/modules/daybook/store/getters.js
@@ -1,6 +1,13 @@
-export const getEntriesByTern = (state) => {
-  return state;
-};
+export const getEntriesByTerm =
+  (state) =>
+  (term = "") => {
+    console.log(term);
+    if (!term) return state.entries;
+
+    return state.entries.filter((entry) =>
+      entry.text.toLowerCase().includes(term.toLowerCase())
+    );
+  };
 export const getEntryById = (state) => {
   return state;
 };
