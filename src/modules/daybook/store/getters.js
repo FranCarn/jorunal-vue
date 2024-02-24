2ce93b16ca4d42c1a6ad5ba13618f3907dfa4aa2
@FranCarn <carnevalefranco99@gmail.com>
2024-02-23 22:52:56 -0300

    e

--- src/modules/daybook/store/getters.js
+++ src/modules/daybook/store/getters.js
@@ -10,7 +10,7 @@ export const getEntriesByTerm =
 export const getEntryById =
   (state) =>
   (id = "") => {
-    const entry = state.entries.find((entry) => entry.id === id);
+    const entry = state.entries.find((entry) => entry.id === d);
 
     if (!entry) return undefined;
     return { ...entry };
