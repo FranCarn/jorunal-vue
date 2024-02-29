ebb5fd45e05aaffce2a676f98dad51b007550129
@FranCarn <carnevalefranco99@gmail.com>
2024-02-28 22:41:22 -0300

    fix settings app

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "./styles/styles.scss";
 import store from "./store";
-import toastify, { settings } from "vue-toastify";
+import toastify from "vue-toastify";
 import "vue-toastify/index.css";
 
 createApp(App)
   .use(store)
-  .use(toastify, { ...settings, position: "top-right", defaultTitle: false })
+  .use(toastify, { position: "top-right", defaultTitle: false })
   .use(router)
   .mount("#app");
