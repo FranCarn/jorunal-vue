417012bbfe03ad21ca5da610ead82ee3bc21f1a0
@FranCarn <carnevalefranco99@gmail.com>
2024-04-03 18:42:06 -0300

    auth mutations tests ready

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -1,7 +1,7 @@
 import createVuexStore from "../../../mocks/mock-store";
 
 describe("test on auth module vuex", () => {
-  test("init state ", () => {
+  test("init state", () => {
     const store = createVuexStore({
       status: "authenticating", // 'authenticated', 'not-authenticated', 'authenticating'
       user: null,
@@ -14,5 +14,52 @@ describe("test on auth module vuex", () => {
     expect(user).toBeNull();
     expect(idToken).toBeNull();
     expect(refreshToken).toBeNull();
+  });
+
+  // Mutations
+  test("Mutations: loginUser", () => {
+    const store = createVuexStore({
+      status: "authenticating",
+      user: null,
+      idToken: null,
+      refreshToken: null,
+    });
+
+    const payload = {
+      user: { name: "Test", email: "testing@test.com" },
+      idToken: "ABC-123",
+      refreshToken: "XYZ-123",
+    };
+
+    store.commit("auth/loginUser", payload);
+
+    const { status, user, idToken, refreshToken } = store.state.auth;
+
+    expect(status).toBe("authenticated");
+    expect(user).toMatchObject(payload.user);
+    expect(idToken).toBe("ABC-123");
+    expect(refreshToken).toBe("XYZ-123");
+  });
+
+  test("Mutations: logout", () => {
+    localStorage.setItem("idToken", "ABC-123");
+    localStorage.setItem("refreshToken", "XYZ-123");
+
+    const store = createVuexStore({
+      status: "authenticated",
+      user: { name: "Test", email: "testing@test.com" },
+      idToken: "ABC-123",
+      refreshToken: "XYZ-123",
+    });
+
+    store.commit("auth/logout");
+
+    const { status, user, idToken, refreshToken } = store.state.auth;
+    expect(status).toBe("not-authenticated");
+    expect(user).toBeFalsy();
+    expect(idToken).toBeFalsy();
+    expect(refreshToken).toBeFalsy();
+    expect(localStorage.getItem("idToken")).toBeFalsy();
+    expect(localStorage.getItem("refreshToken")).toBeFalsy();
   });
 });
